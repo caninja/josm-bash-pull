c98404d2049a3aa4aafb01030bef9e6b9e4b2efd
Fredrik <fredrik@vangenplotz.no>
2021-11-04 10:30:31 +0100

    update

--- start.sh
+++ start.sh
@@ -1,29 +1,55 @@
-#!/bin/bash
+#!/bin/bash 
 
-# latest version number of josm as a variable
+#TODO versioning or replace
+
+input="$1"
 latest=$(curl -sw '\n' "https://josm.openstreetmap.de/latest")
+tested=$(curl -sw '\n' "https://josm.openstreetmap.de/tested")
+offline_latest=$(\ls . | grep josm-latest-*.jar | sed 's/[^0-9]*//g')
+offline_tested=$(\ls . | grep josm-tested-*.jar | sed 's/[^0-9]*//g')
 
-# download the latest josm
-function get_latest () {
-curl -s "https://josm.openstreetmap.de/josm-latest.jar?lang=en" -o josm-latest.jar
-mv josm-latest.jar josm-latest-${latest}.jar
-chmod +x josm-latest-${latest}.jar
+function check_tested_get_tested () {
+if [ -f josm-tested-*.jar ]; then
+  if [ "$tested" !== "$offline_tested" ]; then
+    curl -s "https://josm.openstreetmap.de/josm-tested.jar?lang=en" -o josm-tested-${tested}.jar
+    chmod +x josm-tested-${tested}.jar
+  fi
+else
+    curl -s "https://josm.openstreetmap.de/josm-tested.jar?lang=en" -o josm-tested-${tested}.jar
+    chmod +x josm-tested-${tested}.jar
+fi
 }
 
-# if josm is found here, get version number from it, else download latest josm
-if [ -e josm* ]
-then
-installed=$(ls josm-latest-*.jar | cut -c 13-17)
+function check_latest_get_latest () {
+if [ -f josm-latest-*.jar ]; then
+  if [ "$latest" !== "$offline_latest" ]; then
+    curl -s "https://josm.openstreetmap.de/josm-latest.jar?lang=en" -o josm-latest-${latest}.jar
+    #mv josm-tested.jar josm-latest-${latest}.jar
+    chmod +x josm-latest-${latest}.jar
+  fi
 else
-get_latest
+    curl -s "https://josm.openstreetmap.de/josm-latest.jar?lang=en" -o josm-latest-${latest}.jar
+    chmod +x josm-latest-${latest}.jar
+fi
+}
+
+if [ "nc -dzw1 google.com 443" ]
+then#gotnet
+  if [ "$input" == "latest" ]
+    then
+    check_latest_get_latest
+    java -jar -Xmx6G josm-latest-${latest}.jar
+  else
+    check_tested_get_tested
+    java -jar -Xmx6G josm-tested-${tested}.jar
+  fi
+
+else#notgotnet
+  if [ "$input" == "latest" ]
+    then
+    java -jar -Xmx6G josm-latest-*.jar
+    else
+    java -jar -Xmx6G josm-tested-*.jar
+  fi
 fi
 
-# latest is newer
-if [ $installed != $latest ]
-then
-rm josm-latest-${installed}.jar
-get_latest
-fi
-
-# run josm
-java -jar -Xmx6G josm-latest-${latest}.jar
