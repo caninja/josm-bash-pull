6a3688ad3ad36a3677b00b8f48e3b0fedd8a5a6a
fredrik <fredrik.nilsen.edler@gmail.com>
2021-12-20 20:54:13 +0100

    minor changes

--- start.sh
+++ start.sh
@@ -7,6 +7,14 @@ latest=$(curl -sw '\n' "https://josm.openstreetmap.de/latest")
 tested=$(curl -sw '\n' "https://josm.openstreetmap.de/tested")
 offline_latest=$(\ls . | grep josm-latest-*.jar | sed 's/[^0-9]*//g')
 offline_tested=$(\ls . | grep josm-tested-*.jar | sed 's/[^0-9]*//g')
+Y=$(xrandr --current | grep '*' | uniq | awk '{print $1}' | cut -d 'x' -f2)
+S=1
+
+function check_resolution () {
+if [ "$Y" -ge "1440" ]; then
+S=2
+fi
+}
 
 function check_tested_get_tested () {
 if [ -f josm-tested-*.jar ]; then
@@ -22,7 +30,7 @@ fi
 
 function check_latest_get_latest () {
 if [ -f josm-latest-*.jar ]; then
-  if [ "$latest" !== "$offline_latest" ]; then
+  if ! [ "$latest" == "$offline_latest" ]; then
     curl -s "https://josm.openstreetmap.de/josm-latest.jar?lang=en" -o josm-latest-${latest}.jar
     #mv josm-tested.jar josm-latest-${latest}.jar
     chmod +x josm-latest-${latest}.jar
@@ -33,23 +41,25 @@ else
 fi
 }
 
+check_resolution
+
 if [ "nc -dzw1 google.com 443" ]
 then #gotnet
   if [ "$input" == "latest" ]
     then
     check_latest_get_latest
-    java -jar -Xmx6G josm-latest-${latest}.jar
+    GDK_SCALE=$S java -jar -Xmx12G josm-latest-${latest}.jar
   else
     check_tested_get_tested
-    java -jar -Xmx6G josm-tested-${tested}.jar
+    GDK_SCALE=$S java -jar -Xmx12G josm-tested-${tested}.jar
   fi
 
 else #notgotnet
   if [ "$input" == "latest" ]
     then
-    GDK_SCALE=2 java -jar -Xmx12G josm-latest-*.jar
+     GDK_SCALE=$S java -jar -Xmx12G josm-latest-*.jar
     else
-    GDK_SCALE=2 java -jar -Xmx12G josm-tested-*.jar
+     GDK_SCALE=$S java -jar -Xmx12G josm-tested-*.jar
   fi
 fi
 
